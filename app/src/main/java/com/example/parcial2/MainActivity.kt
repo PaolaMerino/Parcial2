563b1c8c2c1933bdd9e0680ed24eb53b255ca49c
PaolaMerino <113115113+PaolaMerino@users.noreply.github.com>
2022-11-03 20:00:45 -0600

    commit

--- app/src/main/java/com/example/parcial2/MainActivity.kt
+++ app/src/main/java/com/example/parcial2/MainActivity.kt
@@ -4,28 +4,56 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.badge.BadgeDrawable
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
-    lateinit var tvespecie: TextView
-    lateinit var tvsexo: TextView
-    lateinit var tvhabitat: TextView
-    lateinit var ivImgFoto: ImageView
+    private lateinit var tlVistaTabla: TabLayout
+    private lateinit var vpVistaPager: ViewPager2
 
+    private val adaptador by lazy{
+        vpAdaptador(this)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tlVistaTabla = findViewById(R.id.tl_VistaTablero)
+        vpVistaPager = findViewById(R.id.vp_VistaPager)
 
-        tvespecie = findViewById(R.id.tv_especie)
-        tvsexo=findViewById(R.id.tv_sexo)
-        tvhabitat=findViewById(R.id.tv_habitat)
-        ivImgFoto=findViewById(R.id.iv_ImgFoto)
-
-        var bundle:Bundle = getIntent().getExtras()!!
+        vpVistaPager.adapter = adaptador
 
 
+        val mediadorTabla: TabLayoutMediator = TabLayoutMediator(tlVistaTabla,vpVistaPager,
+            TabLayoutMediator.TabConfigurationStrategy{
+                    tab, position ->
+                when(position){
+                    0->{
+
+                        val badge: BadgeDrawable = tab.orCreateBadge
+                        badge.backgroundColor = ContextCompat.getColor(applicationContext,R.color.micolor)
+                        badge.isVisible = true
+
+                    }
+                    1->{
+
+                        val badge: BadgeDrawable = tab.orCreateBadge
+                        badge.backgroundColor = ContextCompat.getColor(applicationContext,R.color.micolor)
+                        badge.number = 10
+                        badge.isVisible = true
+                    }
+                    2->{
 
 
+                        val badge: BadgeDrawable = tab.orCreateBadge
+                        badge.backgroundColor = ContextCompat.getColor(applicationContext,R.color.micolor)
+
+                    }
+
+                }
+            })
+        mediadorTabla.attach()
     }
-
 }
-
